f3b193ca3a867afa4a3ddb911bf4b04f79303ac6
Nicole Ruivo <nicoleapruivo@gmail.com>
2023-09-10 21:18:07 -0300

    small fixes

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker/locale/en'
 
 describe('Sign up', () => {
   const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
-  const password = Cypress.env('USER_PASSWORD')
+  const password = 's3Cre7P@sSw0rd'//Cypress.env('USER_PASSWORD')
 
   it('successfully signs up using confirmation code sent via email', () => {
     cy.fillSignupFormAndSubmit(emailAddress, password)
